d684562b3d31d7cfa2c6480a577177586e461cad
ernestorangel <ernestojrcjunior@gmail.com>
2024-03-11 14:11:58 -0300

    refactor: adding next.js setup manually

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"EGTa9feS5vuXdX+YBaI5NpiDVdArIIsUZkrq7+oHKt4=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <h1>
+      You are at ernestorangel.dev! <br></br>
+      We are under construction! <br></br>
+      Please, return later!
+    </h1>
+  );
+}
