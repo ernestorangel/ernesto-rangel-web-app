c36e0dc63aa5077c671a07ddfce546d7e40da3d4
ernestorangel <ernestojrcjunior@gmail.com>
2025-12-17 11:40:24 -0300

    style: update default language to PT-BR and improve formatting in useLanguageContent utility

--- src/utils/useLanguageContent.js
+++ src/utils/useLanguageContent.js
@@ -1,40 +1,41 @@
 export default function useLanguageContent() {
-    const DEFAULT_LANGUAGE = 'EN-US'
+  const DEFAULT_LANGUAGE = "PT-BR";
 
-    const translateLanguage = {
-        'PT-BR': {
-            profileCard: {
-                name: 'Ernesto Rangel',
-                location: 'Brasil',
-                ocupation: 'Desenvolvedor Front-end',
-                description: 'com uma sólida experiência no mercado \
+  const translateLanguage = {
+    "PT-BR": {
+      profileCard: {
+        name: "Ernesto Rangel",
+        location: "Brasil",
+        ocupation: "Desenvolvedor Front-end",
+        description:
+          "com uma sólida experiência no mercado \
                 e colaborando com equipes ágeis para \
                 desenvolver aplicações web escaláveis \
                 e funcionais combinando boas práticas de \
                 desenvolvimento com criatividade \
                 para entregar experiências \
-                digitais impactantes.',
-                highlightTitle: 'Principais Competências'
-            }
-        },
-        'EN-US': {
-            profileCard: {
-                name: 'Ernesto Rangel',
-                location: 'Brazil',
-                ocupation: 'Front-end Developer',
-                description: 'with a solid work experience \
+                digitais impactantes.",
+        highlightTitle: "Principais Competências",
+      },
+    },
+    "EN-US": {
+      profileCard: {
+        name: "Ernesto Rangel",
+        location: "Brazil",
+        ocupation: "Front-end Developer",
+        description:
+          "with a solid work experience \
                 collaborating with agile teams to \
                 develop scalable and functional web applications \
                 combining good development practices \
                 with creativity to deliver \
-                impactful digital experiences',
-                highlightTitle: 'Main Skills'
-            }
-        }
-    }
+                impactful digital experiences",
+        highlightTitle: "Main Skills",
+      },
+    },
+  };
 
-    const getContent = (lang, component) => (translateLanguage[lang][component])
+  const getContent = (lang, component) => translateLanguage[lang][component];
 
-    return { DEFAULT_LANGUAGE, getContent }
+  return { DEFAULT_LANGUAGE, getContent };
 }
-
